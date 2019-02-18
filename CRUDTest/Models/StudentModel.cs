f172fce85e9f67462a81a0a7c12d34a054d9021c
Apra <Apra@DESKTOP-5RVKAU5>
2019-02-18 22:37:14 +0800

    加入model

---
+++ CRUDTest/Models/StudentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUDTest.Models
+{
+    public class StudentModel
+    {
+        public int ID { get; set; }
+        public int SeatNumber { get; set; }
+        public string Name { get; set; }
+        public string Memo { get; set; }
+    }
+}
